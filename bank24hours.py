c6b90c6779dd5ca06fc401f867bac62972ca717a
ElielSantos <35500688+ElielSantos@users.noreply.github.com>
2023-02-10 12:26:05 -0300

    Add files via upload

--- bank24hours.py
+++ bank24hours.py
@@ -1,30 +1,51 @@
 from datetime import datetime
 
-current_dateTime = datetime.now()
+current_dateTime = datetime.now() #BUSCA HORA/DATA DO IP CONECTADO
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-data = datetime.now()
+current_time = now.strftime("%H:%M:%S") 
+data = datetime.now() 
+
+ 
+print()
+#INPUT PARA DADOS DO CLIENTE
+A = input('NOME COMPLETO ')
+B = int(input('AGENCIA '))
+C = (input('CONTA '))
+D = x = 0000
 
 print()
-print('....BEM-VINDO AO PROGRAMA DO BANCO CIPRESTE....')
-#input para dados cliente
-A = input("NOME COMPLETO:")
-B = int(input('AGENCIA:'))
-C = int(input('CONTA:'))
-
-print('')
 print('......DEMOSTRATIVO DE OPERAÇÃO......')
-print('01010 BANCO 24 HORAS') #IMPRIMIR LOCAL PELO ID
-print('DATA DA IMPRESSÃO =', current_dateTime.year) #DATA DO EXTRATO AQUI
+print('01010 PARK SHOPPING IN BRAZIL...',end='')
+print(current_dateTime.year) #DATA DO EXTRATO AQUI
 print('CIPRESTE BANK S A')
-print("HORA DE BRASILIA =", current_time) #HORA DA IMPRESSAO AQUI 
+print(current_time,"(HORÁRIO DE BRASILIA)") #HORA DA IMPRESSAO AQUI 
 print('....................................') 
+print()
 
-print('SALDO N. 0000000000000') #IMPRIME A QUANTIDADE DE EXTRATO NO DIA NO APP
+print('SALDO N. 000000000-0000') #IMPRIME A QUANTIDADE DE EXTRATO NO DIA NO APP
 print('BANCO CIPRESTE SALDO DA CONTA CORRENTE')
-print('AGENCIA =',B)
-print('CONTA =',C)
+print('AGENCIA =',A, end='')
+print(current_dateTime.year, end='  ')
+print(current_time,)
+print('CONTA = ',B, end='')
 print(A)
 
+print()
+print('(+)SALDO PROVISORIO CONTA = ', end='')
+print(x)
+print('(=)SALDO DISPONIVEL P/SAQUE = ', end='')
+print(x)
+
+print()
+print('informações importantes no verso.')
+print()
 
 print('www.ciprestebank.com.br')
+print('Impressão em papel termossensível com\n'
+'vida útil de 5 anos. Evite contato com\n'
+'plásticos, produtos químicos exposição\n'
+'ao calor, umidade, luz do sol e lâmpadas.')
+print()
+
+#FIM DO PROGRAMA
+
